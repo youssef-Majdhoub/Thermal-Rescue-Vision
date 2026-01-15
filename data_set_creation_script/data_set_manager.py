99bd790b3c07c11f9a07ad03ba2a873fb9ee29c8
youssef <majdoubyoussef79@gmail.com>
2026-01-15 19:56:46 +0100

    the data_set creation logic:running this script will create the data set needed out of the raw data

--- data_set_creation_script/data_set_manager.py
+++ data_set_creation_script/data_set_manager.py
@@ -1,3 +1,7 @@
+import os
+import json
+import pandas as pd, numpy as np
+
 # --- STRICT "LIVING THINGS ONLY" MAPPING ---
 # Class 0 = Humans
 # Class 1 = living creatues
@@ -19,3 +23,40 @@ ID_MAPPING = {
     23: 1,  # Zebra
     24: 1,  # Giraffe
 }
+annotation_path = os.path.abspath(
+    "./archive/FLIR_ADAS_v2/images_thermal_train/coco.json"
+)
+data_set_path = os.path.abspath(
+    "./data_set/human_and_living_creatures_count_data_set.csv"
+)
+with open(annotation_path) as f:
+    annotation = json.load(f)
+length = len(annotation["images"])
+data_set = {
+    "id": np.zeros(length),
+    "file_path": [""] * length,
+    "human_count": np.zeros(length, dtype=int),
+    "living_creature_count": np.zeros(length, dtype=int),
+}
+c = 0
+for image in annotation["images"]:
+    data_set["id"][c] = image["id"]
+    path = image["file_name"]
+    data_set["file_path"][c] = path
+    c += 1
+df = pd.DataFrame(data_set)
+df.index = df["id"]
+df = df.drop(columns=["id"])
+for ann in annotation["annotations"]:
+    image_id = ann["image_id"]
+    category_id = ann["category_id"]
+    if category_id in ID_MAPPING:
+        class_id = ID_MAPPING[category_id]
+        if class_id == 0:
+            df.at[image_id, "human_count"] += 1
+            df.at[
+                image_id, "living_creature_count"
+            ] += 1  # Count humans as living creatures too
+        elif class_id == 1:
+            df.at[image_id, "living_creature_count"] += 1
+df.to_csv(data_set_path, index=True)
