05b718e06935d29bcb384af94f60f45b23434a26
youssef <majdoubyoussef79@gmail.com>
2026-04-05 16:55:16 +0100

    final path fixing

--- data_set_creation_script/download_flir_adas.py
+++ data_set_creation_script/download_flir_adas.py
@@ -5,7 +5,7 @@ import shutil
 
 def download_flir_adas_dataset(output_path=None):
     if output_path is None:
-        output_path = os.path.abspath("./archive1")
+        output_path = os.path.abspath("./archive")
         os.makedirs(output_path, exist_ok=True)
     path = kagglehub.dataset_download(
         "samdazel/teledyne-flir-adas-thermal-dataset-v2",
